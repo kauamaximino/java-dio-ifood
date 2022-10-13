139ed96b5a36bc6aeaa5b153d70f9cbe10b359f8
Kauã Maximino <kaua_maximino@outlook.com>
2022-10-13 19:21:59 -0300

    declaring variables in java

---
+++ java-anatomia-classes-1/anatomia-2/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+  public static void main(String[] args) {
+    final String BR = "Brasil";
+
+    int ano = 2021;
+    ano = 2022;
+
+    final double PI = 3.14;
+
+    System.out.print("BR: " + BR);
+    System.out.print(" ano: " + ano);
+    System.out.print(" PI: " + PI);
+  }
+}
