652ba71e37555d15b4762f47f8f3ab1754bef37f
Kauã Maximino <kaua_maximino@outlook.com>
2022-10-21 20:44:41 -0300

    feat: :sparkles: understanding anatomy of classes

---
+++ java-anatomia-classes-1/anatomia-3/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+class MinhaClasse {
+  public static void main(String[] args) {
+    String meuNome = "Kauã";
+    int idade = 18;
+    Double altura = 1.80;
+
+    System.out.println(nomeCompleto("Kauã","Maximino"));
+  }
+
+  public static String nomeCompleto(String nome, String sobrenome) {
+    return nome.concat(" ").concat(sobrenome);
+  }
+}
