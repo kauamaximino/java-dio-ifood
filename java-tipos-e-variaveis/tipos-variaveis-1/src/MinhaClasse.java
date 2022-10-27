e9df5c486b9b94d46547bd270f6a4cf4c8839f68
Kauã Maximino <kaua_maximino@outlook.com>
2022-10-27 19:59:12 -0300

    feat: :sparkles: variable final

--- java-tipos-e-variaveis/tipos-variaveis-1/src/MinhaClasse.java
+++ java-tipos-e-variaveis/tipos-variaveis-1/src/MinhaClasse.java
@@ -2,5 +2,9 @@ public class MinhaClasse {
     public static void main(String[] args) {
         String hello = "Hello World";
         System.out.println(hello);
+
+        final int ANO = 2021;
+        
+        System.out.println(ANO);
     }
 }
