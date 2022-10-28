0b16b753738ad47276b5b3c948eea5dc2cfe4d18
Kauã Maximino <kaua_maximino@outlook.com>
2022-10-27 21:11:37 -0300

    feat: :sparkles: operators in java

---
+++ java-operadores/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+  
+  public static void main(String[] args) {
+    // Concatenação
+    String NameComplete = "Kauã" + " " + "Maximino";
+
+    System.out.println(NameComplete);
+
+    // Operadores aritméticos
+    int x = 10;
+    x = -x;
+
+    System.out.println(x);
+
+    int y = -10;
+    y = +y;
+
+    System.out.println(y);
+
+    int numero = 10;
+    numero += numero;
+    System.out.println(numero);
+  }
+  
+}
