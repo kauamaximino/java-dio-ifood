8697b00f624ca63dccb88485cbe8bc671c0fb5aa
Kauã Maximino <kaua_maximino@outlook.com>
2022-11-04 18:49:18 -0300

    fix: :bug: class name altered

--- java-exercicios-array/array02/Vogais.java
+++ java-exercicios-array/array02/Vogais.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
-public class Consoantes {
+public class Vogais {
   public static void main(String[] args) {
     String[] vetor = { "a", "a", "a", "a", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
 
-    int consoantes = 0;
+    int vogais = 0;
     List<String> list = new ArrayList<String>();
 
     for (int i = 0; i < vetor.length; i++) {
       if (vetor[i].equals("a") || vetor[i].equals("e") || vetor[i].equals("i") || vetor[i].equals("o")
           || vetor[i].equals("u")) {
-            consoantes++;
+            vogais++;
             list.add("Consoante: " + vetor[i]);
           }
 
     }
 
-    System.out.println("Consoantes: " + consoantes);
+    System.out.println("Consoantes: " + vogais);
     System.out.println("Todas as consoantes: " + list);
   }
 }
