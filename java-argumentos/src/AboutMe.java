67fc447c53ec8a2c5ef6a039d88368a97523cc0a
Kauã Maximino <kaua_maximino@outlook.com>
2022-11-03 21:20:15 -0300

    feat: :sparkles: while in Java

--- java-argumentos/src/AboutMe.java
+++ java-argumentos/src/AboutMe.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 
 public class AboutMe {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+      System.out.println("Qual é o seu nome?");
+      String nome = scanner.next();
 
-    System.out.println("Qual é o seu nome?");
-    String nome = scanner.next();
+      System.out.println("Qual é o seu sobrenome?");
+      String sobrenome = scanner.next();
 
-    System.out.println("Qual é o seu sobrenome?");
-    String sobrenome = scanner.next();
+      System.out.println("Qual é a sua idade?");
+      int idade = scanner.nextInt();
 
-    System.out.println("Qual é a sua idade?");
-    int idade = scanner.nextInt();
+      System.out.println("Qual é a sua altura?");
+      double altura = scanner.nextDouble();
 
-    System.out.println("Qual é a sua altura?");
-    double altura = scanner.nextDouble();
-
-
-    System.out.println("Olá, " + nome + " " + sobrenome + "!");
-    System.out.println("Você tem " + idade + " anos e " + altura + " metros de altura.");
+      System.out.println("Olá, " + nome + " " + sobrenome + "!");
+      System.out.println("Você tem " + idade + " anos e " + altura + " metros de altura.");
+    }
   }
 }
