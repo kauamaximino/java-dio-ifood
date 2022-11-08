f6c602c27d91fdc07421bf26251c9ce4eafd6f75
Kauã Maximino <kaua_maximino@outlook.com>
2022-11-08 13:07:26 -0300

    feat: :sparkles: constructor

---
+++ java-POO/Carro.java
@@ -0,0 +1,13 @@
+public class Carro {
+  String marca;
+  String modelo;
+  int ano;
+  String cor;
+
+  public void insertDate(String marca, String modelo, int ano, String cor) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.cor = cor;
+  }
+}
